01a90f001c84235c98c7490d525998fc52e617aa
HasbianNur <kitaraja600@gmail.com>
2023-02-01 11:25:08 +0800

    Update Tampilan Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,33 +8,39 @@
     <link rel="stylesheet" href="{{ URL::asset('css/index.css'); }}">
 </head>
 <body>
-    <nav class="navbar navbar-expand-custom navbar-mainbg">
-        <a class="navbar-brand navbar-logo" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars text-white"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+    <nav class="nav">
+        <div class="nav-main">
+            <div class="logo">MOLN<span>UI</span></div>
+            <ul class="nav-links">
+                <li class="nav-link"><a href="">Home</a></li>
+                <li class="nav-link"><a href="">Our Team</a></li>
+                <li class="nav-link dropdown"><a href="" class="dropdown">Services <i class="bi bi-chevron-compact-down"></i></a>
+                <ul class="dropdown-list">
+                    <li class="nav-link"><a href="">Service 1</a>
+                    <li class="nav-link"><a href="">Service 2</a>
+                </ul>
                 </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Components</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Calendar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Charts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>Documents</a>
-                </li>
+                <li class="nav-link"><a href="">Blog</a></li>
             </ul>
+        </div>
+
+        <div class="cta">
+            <button class="btn btn-secondary">Log In</button>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+        <div class="menu">
+          <button class="btn btn-primary menu"><i class="bi-list"></i></button>
+          <ul class="nav-mobile">
+                <li class="nav-link"><a href="">Home</a></li>
+                <li class="nav-link"><a href="">Our Team</a></li>
+                <li class="nav-link dropdown"><a href="" class="dropdown">Services <i class="bi bi-chevron-compact-down"></i></a>
+                <ul class="dropdown-list">
+                    <li class="nav-link"><a href="">Service 1</a>
+                    <li class="nav-link"><a href="">Service 2</a>
+                </ul>
+                </li>
+                <li class="nav-link"><a href="">Blog</a></li>
+          </ul>
         </div>
     </nav>
     <script src="{{ URL::asset('js/index.js'); }}"></script>
