dc35c4725872fff9a71c3282f0fd644c29f09350
HasbianNur <kitaraja600@gmail.com>
2023-02-02 12:03:15 +0800

    (update dikit)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,6 @@
 @extends('template.index')
 @section('content')
-
+<p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat est illo ratione similique, quibusdam rerum corporis placeat eius! Velit sed ea necessitatibus veritatis dignissimos illo exercitationem iusto, doloribus unde aut atque tenetur magni laboriosam blanditiis minus ex quam illum sunt animi veniam architecto fugiat ullam ipsum? Officiis, delectus sit. Laborum laudantium voluptatibus debitis blanditiis beatae eos ut maxime odio iste inventore eaque quos earum quod voluptas, aut nisi porro at quidem magnam temporibus. Id eius, inventore blanditiis molestiae sit dolor cumque quibusdam pariatur expedita, mollitia sunt vel temporibus, omnis dolorem accusantium sequi dolorum! Error exercitationem consequuntur molestiae id iure assumenda!
+</p>
 @endsection
--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -6,13 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{ URL::asset('css/index.css'); }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css'); }}">
     <link rel="stylesheet" href="{{ URL::asset('css/sidebar.css'); }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 <body>
     @include('template.navbar')
     @include('template.sidebar')
-    @yield('content')
+    <div class="main-content">
+        @yield('content')
+    </div>
 
     <script src="{{ URL::asset('js/index.js'); }}"></script>
 </body>
