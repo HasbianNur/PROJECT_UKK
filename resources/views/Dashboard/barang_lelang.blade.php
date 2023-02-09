c1bfb36eaf7215cfaf8a9c36fcf3a73005bcb790
CharlyJoezer <charlyjoezer30@gmail.com>
2023-02-09 09:05:02 +0800

    membuat tampilan form input barang lelang

--- resources/views/Dashboard/barang_lelang.blade.php
+++ resources/views/Dashboard/barang_lelang.blade.php
@@ -7,7 +7,7 @@
 </div>
 
 <div class="content">
-    <div class="button-create-barang">
+    <div class="button-create-barang" data-bs-toggle="modal" data-bs-target="#exampleModal">
         <span>Lelang Barang</span>
     </div>
 
@@ -17,5 +17,89 @@
         </div>
     </div>
 </div>
+
+<div class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+    <div class="logo">Ternak <span style="color: rgb(0, 187, 255);">Lelang</span></div>
+    <h1 class="modal-title fs-5" id="exampleModalLabel"></h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+
+            <div class="wrapper-input">
+                <form action="">
+                    <table class="table tabel-input-barang">
+                        <tbody>
+                          <tr class="input-barang-box">
+                            <td>Nama Barang</td>
+                            <td>:</td>
+                            <td><input type="text" id="nama-barang" name="nama" placeholder="Nama Barang"></td>
+                          </tr>
+                          <tr class="input-barang-box">
+                            <td>Harga Awal</td>
+                            <td>:</td>
+                            <td><input type="number" id="harga-awal" name="harga" placeholder="Rp."></td>
+                          </tr>
+                          <tr class="input-barang-box">
+                            <td>Tanggal Lelang</td>
+                            <td>:</td>
+                            <td><input type="text" style="background: #ccc;border: 1px solid #ccc;border-radius:5px;" readonly id="input-tanggal-lelang" name="tanggal"></td>
+                          </tr>
+                          <tr class="input-barang-box">
+                            <td>Deskripsi Barang</td>
+                            <td>:</td>
+                            <td>
+                                <textarea type="text"  id="input-deskripsi-lelang" name="deskripsi"></textarea>
+                            </td>
+                          </tr>
+                          <tr class="input-barang-box">
+                            <td>Gambar Barang</td>
+                            <td>:</td>
+                            <td>
+                                <input class="form-control" type="file" id="formFile">
+                                <small style="color:red;font-style:italic;font-size:12px;">*tidak boleh kosong!</small>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                </form>
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    $(document).ready(function(){
+        const date = new Date();
+        $('#input-tanggal-lelang').val(date.getDate()+'-'+(date.getMonth() + 1)+'-'+date.getFullYear())
+    })
+  </script>
 
 @endsection
--- resources/views/Dashboard/template/template.blade.php
+++ resources/views/Dashboard/template/template.blade.php
@@ -8,6 +8,8 @@
     <link href="http://fonts.cdnfonts.com/css/nexa-bold" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/css/dashboard.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 </head>
     <body class="flex">
     
@@ -16,6 +18,8 @@
         @yield('content')
     </section>
 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"
         integrity="sha256-ErZ09KkZnzjpqcane4SCyyHsKAXMvID9/xwbl/Aq1pc=" crossorigin="anonymous"></script>
     <script src="/js/dashboard.js"></script>
